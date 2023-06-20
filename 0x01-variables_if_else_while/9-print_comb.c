a2ef2aa9727be698134f3bfebe033f50b3eba11e
Korede <obolaji.anibaba@gmail.com>
2023-06-19 21:18:51 -0700

    Variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 	for (x = 48; x <= 57; x++)
 	{
 		putchar(x);
-		if (x != 58)
+		if (x != 57)
 		{
 			putchar(',');
 			putchar(' ');
-		}		
+		}	
 	}
 	putchar('\n');
 	return(0);
