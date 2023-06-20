fa3af9a2018a5dbf47e19c84a3803858f53fbb8c
Korede <obolaji.anibaba@gmail.com>
2023-06-19 19:58:42 -0700

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 'a';
+	while (x <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
