d50d5a61d2f1a12a868424cc8a8d8245dcb231f4
Korede <obolaji.anibaba@gmail.com>
2023-06-26 18:09:53 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	int slength = 0;
+
+	while(*s!='\0')
+	{
+           slength++;
+           s++;
+	}
+   return slength;
+}
