c219f887e0c3e1f0961b357579832334624133f5
Korede <obolaji.anibaba@gmail.com>
2023-06-29 18:00:01 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i]= src[j];
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
