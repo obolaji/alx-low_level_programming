62069dafea655d908df7afd28699fe675305d8b7
Korede <obolaji.anibaba@gmail.com>
2023-07-03 18:46:20 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: first value
+ * @accept: second value
+ *
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, k = 0;
+	unsigned int j = 0;
+
+	while (s[i] != ' ' && s[i] != '\0')
+	{
+		while (accept[k] != '\0')
+		{
+			if (s[i] == accept[k])
+				j++;
+			k++;
+		}
+		i++;
+	}
+	return (j);
+}
