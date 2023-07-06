8622de242175c9433e9c7e3514fd9953b713a6ea
Korede <obolaji.anibaba@gmail.com>
2023-07-05 19:30:06 -0700

    0x08. C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 
 /**
+ * prime - checks if number is prime
+ * @i: integer
+ * @j: integer
+ * Return: Always 0.
+ */
+
+int prime(int i, int j)
+{
+	if (i == j)
+		return (1);
+	if (i % j == 0)
+		return (0);
+	return (prime(i, j + 1));
+}
+/**
  * is_prime_number - checks if a number is prime
  * @n: Integer
  *
@@ -12,20 +27,4 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 	return (prime(n, 2));
-}
-
-/**
- * prime - checks if number is prime
- * @i: integer
- * @j: intege
- * Return: Always 0.
- */
-
-int prime(int i, int j)
-{
-	if (i == j)
-		return (1);
-	else if (i % j == 0)
-		return (0);
-	return (prime(i, j + 1));
 }
