b2d9ce4b8ddd2397f29b225ddbc438dae606c699
Korede <obolaji.anibaba@gmail.com>
2023-07-11 20:54:39 -0700

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int k;
+
+	for (k = 0; k < height; k++)
+	{
+		free(grid[k]);
+	}
+	free(grid);
+}
+
