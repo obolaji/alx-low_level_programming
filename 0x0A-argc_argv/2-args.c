6bc0dac3de066d16b6b497e29348b21c6505b3e2
Korede <obolaji.anibaba@gmail.com>
2023-07-12 18:24:30 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @a: int
+ * @b: char
+ * Return: 0
+ */
+
+int main(int a, char *b[])
+{
+	int i;
+
+	for (i = 0; i < a; i++)
+		printf("%s\n", b[i]);
+
+	return (0);
+}
