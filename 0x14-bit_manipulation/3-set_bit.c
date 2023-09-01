4282b331047369297c7ed46dd4c516b807dedd33
obolaji <obolaji.anibaba@gmail.com>
2023-08-31 17:56:56 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_bit - value of a bit to 1 at a given index
+ * @n: pointer
+ * @index: integer
+ *
+ * Return: Always 0.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1;
+
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	i <<= index;
+
+	*n |= i;
+	return (1);
+}
