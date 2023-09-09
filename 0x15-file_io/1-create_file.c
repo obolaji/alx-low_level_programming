61af51a5abd90a432a5ceff4e02a0298a1a44383
obolaji <obolaji.anibaba@gmail.com>
2023-09-08 19:43:45 -0700

    15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	wr = write(file, text_content, i);
 	if (wr == -1)
-                return (-1);
+		return (-1);
 
 	close(file);
 	return (1);
