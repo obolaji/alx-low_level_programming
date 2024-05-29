2d3c4b12215d81ec91e8bcd589a4548ff5475abd
Obolaji <obolaji.anibaba@gmail.com>
2024-05-29 13:54:51 -0700

    push all

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return(0);
+}
