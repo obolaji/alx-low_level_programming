6993e070a18f4988389bdde9960eb5955a7727a8
Obolaji <obolaji.anibaba@gmail.com>
2024-08-01 06:16:24 -0700

    0x02c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table -  prints the 9 times table, starting with 0.
+ *
+ * Return: Always 0.
+ *
+ *
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+
+			if (k <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
