062719af6b304200256c5fa4033d668c0e1bfd5e
Obolaji <obolaji.anibaba@gmail.com>
2024-08-03 15:15:30 -0700

    0x02c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: Always 0.
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
