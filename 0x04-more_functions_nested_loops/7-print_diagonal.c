b532f2712d8eb9acc9477543ba1817d051cf83bd
Obolaji <obolaji.anibaba@gmail.com>
2024-08-07 13:17:16 -0700

    0x04

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (i == j)
+				_putchar('\\');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
