694260b6bb22fb7db5d061b5b072a30ec9f7bac7
Obolaji <obolaji.anibaba@gmail.com>
2024-08-08 09:39:04 -0700

    0x04c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include "main.h"
 /**
  * main - FizzBuzz
@@ -12,13 +12,21 @@ int main(void)
 	 for (i = 1; i <= 100; i++)
 	 {
 		 if (i % 3 == 0 && i % 5 != 0)
+		 {
 			 printf("Fizz ");
+		 }
 		 else if (i % 5 == 0 && i % 3 != 0)
+		 {
 			 printf("Buzz ");
+		 }
 		 else if (i % 3 == 0 && i % 5 == 0)
+		 {
 			 printf("FizzBuzz ");
+		 }
 		 else if (i == 1)
+		 {
 			 printf("%d ", i);
+		 }
 		 else
 			 printf("%d ", i);
 	 }
