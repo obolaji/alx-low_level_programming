0cfcefa4187513cb3c10d5e2223cf19caba2223c
Obolaji <obolaji.anibaba@gmail.com>
2024-08-08 12:44:51 -0700

    0x04c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,9 +13,9 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				if (i == j)
 					_putchar('\\');
