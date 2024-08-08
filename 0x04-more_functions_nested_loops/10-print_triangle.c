ae4d9e13e0a2b45ddf29533324abd6687dd0345a
Obolaji <obolaji.anibaba@gmail.com>
2024-08-08 14:29:15 -0700

    0x04c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = 0; j <= size; j++)
+			for (j = 1; j <= size; j++)
 			{
 				if (j <= i)
 					_putchar('#');
