86089a70a456681528b36a0780064c41902c3d4d
Obolaji <obolaji.anibaba@gmail.com>
2024-08-16 06:07:18 -0700

    0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 /**
  * main - prints the name of the program
+ * @argc: integer
+ * @argv: poimter
  *
  * Return: Always 0.
  *
@@ -10,5 +12,5 @@ int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return 0;
+	return (0);
 }
